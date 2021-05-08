f49a4957b97c2e8653f2e4843c1058e8d12ff778
Dillon Kearns <dillon@dillonkearns.com>
2021-05-07 20:19:09 -0700

    Try running hardcoded git clone on a sample project.

--- functions/run.js
+++ functions/run.js
@@ -1,7 +1,21 @@
+const {  execSync } = require("child_process");
+const fs = require('fs')
+
+
 exports.handler = async function(request) {
+    const output = generateDocs()
+    console.log(output.toString());
+    
     return {
         statusCode: 200,
-        body: 'Hello'
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: fs.readFileSync('project/docs.json').toString()
     }
     
+}
+
+function generateDocs() {
+    return execSync("rm -rf project && git clone https://github.com/dillonkearns/elm-rss.git --branch master --single-branch project && cd project && elm make --docs docs.json")
 }
